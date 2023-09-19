99051fe90b48109403cfb804b0ab1b93a6e278cf
chaeyeon0101 <87770634+chaeyeon0101@users.noreply.github.com>
2023-09-20 01:50:21 +0900

    Week1 과제 제출

---
+++ Week1/GDSC_1st_HW/src/main/java/org/example/Animal.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public abstract class Animal {
+    public abstract void speak();
+}
+
+class Cat extends Animal{
+    public void speak(){
+        System.out.println("cat says Meow!");
+    }
+}
+
+class Dog extends  Animal{
+    public void speak(){
+        System.out.println("dog says Woof!");
+    }
+}
